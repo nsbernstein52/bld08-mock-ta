168b96ff4605edc649d04e4a2d305dce7e947ec5
nsbernstein52 <57200889+nsbernstein52@users.noreply.github.com>
2020-03-06 07:23:17 -0700

    Change path from /App/ to /app/

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
+import App from './components/app/App'; // /app/
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
