24dcf6c59e84ac0dc8bff2e644024127c3303158
nsbernstein52 <57200889+nsbernstein52@users.noreply.github.com>
2020-03-06 07:21:11 -0700

    Add header comments function structure

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,49 @@
 // server/db/db.js
 // pg
+// functions:
+//   addAnimal, tbd
+//   deleteAnimal, tbd
+//   getAllAnimals, tbd
+//   getAnimal, tbd
+//   getAnimalByLocation, tbd
+//   getAnimalByType, tbd
+//   updateAnimalLocation tbd
 
 const { Pool } = require('pg');
+
+const pool = new Pool({
+  database: 'farm',
+  user: 'postgres',
+  password: 'Psa2020s'
+});
+
+console.log('db: Entering');
+
+//   addAnimal, tbd
+
+//   deleteAnimal, tbd
+
+//   getAllAnimals, tbd
+
+//   getAnimal, tbd
+
+//   getAnimalByLocation, tbd
+
+//   getAnimalByType, tbd
+
+//   updateAnimalLocation tbd
+
+console.log('db: Exiting');
+
+module.exports = {
+  addAnimal,
+  deleteAnimal,
+  getAllAnimals,
+  getAnimal,
+  getAnimalByLocation,
+  getAnimalByType,
+  updateAnimalLocation
+} 
 
 // //// original code
 // const { Pool } = require('pg');
