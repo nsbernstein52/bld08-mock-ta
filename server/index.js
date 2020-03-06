68db83748c30174a63c17ebc6efcf0806f74ea59
nsbernstein52 <57200889+nsbernstein52@users.noreply.github.com>
2020-03-06 07:22:46 -0700

    Add body-parser, cors, logs: enter/exit

--- server/index.js
+++ server/index.js
@@ -2,10 +2,19 @@
 // port 8080
 
 const express = require('express');
-const cors = require('cors')
-
 const app = express();
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+const cors = require('cors')
+app.use(cors);
+
 const port = 8080;
+
+console.log('i: Entering');
+
+console.log('i: Exiting');
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
